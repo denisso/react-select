4f1ad1fad65ad44daf54b2b6497065224dd17faf
Denis <like-nix@mail.ru>
2024-11-30 22:39:10 +0300

    show full code

--- stories/Select01-Portal.stories.tsx
+++ stories/Select01-Portal.stories.tsx
@@ -38,5 +38,67 @@ export const Default: Story = {
       </div>
     ),
   ],
+  parameters: {
+    docs: {
+      source: {
+        code: `
+const CustomButton = () => {
+  return (
+    <Button
+      placeholder="placeholder"
+      className={styles.button}
+      styles={{ open: styles.open }}
+    />
+  );
+};
 
+type OptionType = {
+  label: string;
+  value: string;
+};
+
+type Props = {
+  options: OptionType[];
+};
+
+const CustomSelect = ({ options }: Props) => {
+  return (
+    <Select className={styles.select}>
+      <CustomButton />
+      <Menu
+        className={styles.menu}
+        emptyValue={"zero"}
+        portal={true}
+        styles={{ open: styles.open }}
+      >
+        <Option
+          label={"empty option"}
+          value={"zero"}
+          className={styles.option}
+        />
+        {options.map(({ label, value }) => (
+          <Option
+            label={label}
+            value={value}
+            key={value}
+            className={styles.option}
+            styles={{ selected: styles.selected }}
+          />
+        ))}
+      </Menu>
+    </Select>
+  );
+};
+// Usage
+<CustomSelect
+  options={[
+    { label: 'Option 1', value: 'value1' },
+    { label: 'Option 2', value: 'value2' },
+  ]}
+/>
+
+  `,
+      },
+    },
+  },
 };
